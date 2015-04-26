49b0bd256b3f4d8848cfe86668f1c4230e0277d8
Eric Schrijver <eric@ericschrijver.nl>
2015-04-26 11:34:21 -0400

    JavaScript to enable galleries

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ $container.masonry({
     columnWidth: 100,
     itemSelector: '.case'
 });
+
+// GALLERY-BUTTON
+// cf http://jsfiddle.net/lfurter/QJftM/
+$('.next').click(function () {
+    var $current = $(this).parent().parent().find('.image-gallery:not(.hidden)');
+    var $next = $current.next();
+    if ($next.hasClass("hidden")) {
+        $current.addClass("hidden");
+        $next.removeClass("hidden");
+    }
+});
+$('.previous').click(function () {
+    var $current = $(this).parent().parent().find('.image-gallery:not(.hidden)');
+    var $previous = $current.prev();
+    if ($previous.hasClass("hidden")) {
+        $current.addClass("hidden");
+        $previous.removeClass("hidden");
+    }
+});
